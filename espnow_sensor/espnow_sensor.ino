454e205744cfa8fcacd11902540932481feed266
Germán Martín <gmag11@gmail.com>
2019-02-01 16:43:05 +0100

    First commit

---
+++ espnow_sensor/espnow_sensor.ino
@@ -0,0 +1,35 @@
+#include <WifiEspNow.h>
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#elif defined(ESP32)
+#include <WiFi.h>
+#endif
+
+
+void initWiFi () {
+    WiFi.persistent (false);
+    WiFi.mode (WIFI_AP);
+    WiFi.softAP ("ESPNOW", nullptr, 3);
+    WiFi.softAPdisconnect (false);
+
+    Serial.print ("MAC address of this node is ");
+    Serial.println (WiFi.softAPmacAddress ());
+}
+
+void initEspNow () {
+    bool ok = WifiEspNow.begin ();
+    if (!ok) {
+        Serial.println ("WifiEspNow.begin() failed");
+        ESP.restart ();
+    }
+}
+
+void setup () {
+    //***INICIALIZACIÓN DEL PUERTO SERIE***//
+    Serial.begin (115200); Serial.println (); Serial.println ();
+
+}
+
+void loop () {
+
+}
