40d3ba86c460756f8db2711c8d7bc1025579dadb
Germán Martín <gmag11@gmail.com>
2019-03-22 12:47:35 +0100

    Document Node class

--- src/NodeList.h
+++ src/NodeList.h
@@ -1,4 +1,10 @@
-// NodeList.h
+/**
+  * @file NodeList.h
+  * @version 0.0.1
+  * @date 09/03/2019
+  * @author German Martin
+  * @brief EnigmaIoT sensor node management structures
+  */
 
 #ifndef _NODELIST_h
 #define _NODELIST_h
@@ -9,46 +15,85 @@
 	#include "WProgram.h"
 #endif
 
+/**
+  * @brief State definition for nodes
+  */
 enum node_status {
-    UNREGISTERED,
-    INIT,
-    WAIT_FOR_SERVER_HELLO,
-    WAIT_FOR_KEY_EXCH_FINISHED,
-    WAIT_FOR_CIPHER_FINISHED,
-    WAIT_FOR_DOWNLINK,
-    REGISTERED,
-    SLEEP
+    UNREGISTERED, /**< Node is not registered. This is te initial state of every node */
+    INIT, /**< Node is starting registration by ClientHello message */
+    WAIT_FOR_SERVER_HELLO, /**< Node sent ClientHello message, now it is waiting for ServerHello */
+    WAIT_FOR_KEY_EXCH_FINISHED, /**< Gateway sent ServerHello message, now it is waiting for KeyExchangeFinished from Node*/
+    WAIT_FOR_CIPHER_FINISHED, /**< Node sent KeyExchangeFinished message, now it is waiting for CipherFinished */
+    WAIT_FOR_DOWNLINK, /**< Node sent a data message, now it is waiting for downlink data */
+    REGISTERED, /**< Node is registered and its key is valid */
+    SLEEP /**< Node is in sleep mode */
 };
 
-typedef enum node_status status_t;
+typedef enum node_status status_t; ///< @brief Node state
 
+/**
+  * @brief Struct that define node fields. Used for long term storage needs
+  */
 struct node_instance {
-    uint8_t mac[6];
-    uint16_t nodeId;
-    uint8_t key[32];
-    uint16_t lastMessageCounter;
-    time_t keyValidFrom;
-    time_t lastMessageTime;
-    status_t status = UNREGISTERED;
-    bool keyValid = false;
-    bool sleepyNode = true;
+    uint8_t mac[6]; ///< @brief Node address
+    uint16_t nodeId; ///< @brief Node identifier asigned by gateway
+    uint8_t key[32]; ///< @brief Shared key
+    uint16_t lastMessageCounter; ///< @brief Last message counter state for specific Node
+    time_t keyValidFrom; ///< @brief Last time that Node and Gateway agreed a key
+    time_t lastMessageTime; ///< @brief Last time a message was received by Node
+    status_t status = UNREGISTERED; ///< @brief Node state
+    bool keyValid = false; ///< @brief Node shared key valid
+    bool sleepyNode = true; ///< @brief Node sleepy definition
 };
 
 typedef struct node_instance node_t;
 
+/**
+  * @brief Class definition for a single sensor Node
+  */
 class Node {
 public:
+    /**
+      * @brief Plain constructor
+      * @return Returns a new unregistered Node instance
+      */
     Node ();
+
+    /**
+      * @brief Constructor that initializes data from another Node data
+      * @param nodeData `node_instance` struct that contains initalization values for new Node
+      * @return Returns a new Node instance with same data as given `node_instance` struct
+      */
     Node (node_t nodeData);
+
+    /**
+      * @brief Gets address from Node
+      * @return Returns a pointer to Node address
+      */
     uint8_t *getMacAddress () {
         return mac;
     }
+
+    /**
+      * @brief Gets Node identifier
+      * @return Returns Node identifier
+      */
     uint16_t getNodeId () {
         return nodeId;
     }
+
+    /**
+      * @brief Sets a new Node identifier
+      * @param nodeId New nodeId value
+      */
     void setNodeId (uint16_t nodeId) {
         this->nodeId = nodeId;
     }
+
+    /**
+      * @brief Gets Node encryption key
+      * @return Returns a pointer to Node encryption key
+      */
     uint8_t *getEncriptionKey () {
         return key;
     }
@@ -110,15 +155,15 @@ public:
 
 protected:
 #define KEYLENGTH 32
-    uint8_t mac[6];
-    uint8_t key[KEYLENGTH];
-    uint16_t lastMessageCounter;
-    uint16_t nodeId;
-    timer_t keyValidFrom;
-    bool keyValid;
+    uint8_t mac[6]; ///< @brief Node address
+    uint8_t key[KEYLENGTH]; ///< @brief Shared key
+    uint16_t lastMessageCounter; ///< @brief Last message counter state for specific Node
+    uint16_t nodeId; ///< @brief Node identifier asigned by gateway
+    timer_t keyValidFrom; ///< @brief Last time that Node and Gateway agreed a key
+    bool keyValid; ///< @brief Node shared key valid
     status_t status = UNREGISTERED;
-    timer_t lastMessageTime;
-    bool sleepyNode = true;
+    timer_t lastMessageTime; ///< @brief Node state
+    bool sleepyNode = true; ///< @brief Node sleepy definition
 
     friend class NodeList;
 };
@@ -153,7 +198,7 @@ public:
     void printToSerial (Stream *port);
 
 protected:
-    Node nodes[NUM_NODES];
+    Node nodes[NUM_NODES]; ///< @brief Static Node array that holds maximum number of supported nodes 
 
 };
 
