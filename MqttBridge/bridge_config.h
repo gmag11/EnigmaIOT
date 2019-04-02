b6962b4818c38cc97076f39e5e75109eba20b024
Germán Martín <gmag11@gmail.com>
2019-04-02 23:51:33 +0200

    Bridge config file

---
+++ MqttBridge/bridge_config.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef BRIDGE_CONFIG_H
+#define BRIDGE_CONFIG_H
+
+#define SSID "SSID"
+#define PASSWD "PASSWD"
+const char* mqtt_server = "mqtt_server";
+#define MQTT_USER "MQTT_USER"
+#define MQTT_PASS "MQTT_PASS"
+#endif
