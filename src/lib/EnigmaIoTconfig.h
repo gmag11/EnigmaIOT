deb5625a6abfe11f2a9438995bfef2103abf80f3
Germán Martín <gmag11@gmail.com>
2019-04-19 17:40:32 +0200

    Move network key to Crypto config block

--- src/lib/EnigmaIoTconfig.h
+++ src/lib/EnigmaIoTconfig.h
@@ -19,13 +19,13 @@
 static const uint16_t RECONNECTION_PERIOD = 2500;
 static const uint16_t DOWNLINK_WAIT_TIME = 500;
 
-static const uint8_t NETWORK_KEY[32] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
 
 //Crypro configuration
-const uint8_t KEY_LENGTH = 32; ///< @brief Key length used by selected crypto algorythm
+const uint8_t KEY_LENGTH = 32; ///< @brief Key length used by selected crypto algorythm. The only tested value is 32. Change it only if you know what you are doing
 const uint8_t IV_LENGTH = 16; ///< @brief Initalization vector length used by selected crypto algorythm
 #define BLOCK_CYPHER Speck
 #define CYPHER_TYPE CFB<BLOCK_CYPHER>
+static const uint8_t NETWORK_KEY[32] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
 
 //Debug
 #define DEBUG_ESP_PORT Serial
