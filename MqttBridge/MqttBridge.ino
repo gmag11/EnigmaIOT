4cad8b99fb906ede37be7afd3aa13fa2ae4a5713
Germán Martín <gmag11@gmail.com>
2019-04-26 22:35:01 +0200

    Make bridge read incoming MQTT messages

--- MqttBridge/MqttBridge.ino
+++ MqttBridge/MqttBridge.ino
@@ -21,26 +21,66 @@
 #define PASSWD "passwd"
 #endif
 
+//#define BRIDGE_DEBUG
+
 const char* BASE_TOPIC = "enigmaiot";
 
+#ifdef SECURE_MQTT
+BearSSL::X509List cert (DSTrootCA);
+WiFiClientSecure espClient;
+#else
 WiFiClient espClient;
+#endif
 PubSubClient client (espClient);
+
+void onDlData (const char* topic, byte* payload, unsigned int length) {
+	Serial.printf ("*%s;", topic);
+	for (int i = 0; i < length; i++) {
+		Serial.print ((char)(payload[i]));
+	}
+	Serial.println ();
+}
+
+#ifdef SECURE_MQTT
+void setClock () {
+	configTime (2 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+#ifdef BRIDGE_DEBUG
+	Serial.print ("Waiting for NTP time sync: ");
+	time_t now = time (nullptr);
+	while (now < 8 * 3600 * 2) {
+		delay (500);
+		Serial.print (".");
+		now = time (nullptr);
+	}
+	Serial.println ("");
+	struct tm timeinfo;
+	gmtime_r (&now, &timeinfo);
+	Serial.print ("Current time: ");
+	Serial.print (asctime (&timeinfo));
+#endif
+}
+#endif
 
 void reconnect () {
     // Loop until we're reconnected
     while (!client.connected ()) {
         Serial.print ("Attempting MQTT connection...");
         // Create a random client ID
-        String clientId = "ESP8266Client-";
-        clientId += String (random (0xffff), HEX);
+        String clientId = BASE_TOPIC + String (ESP.getChipId (), HEX);
+		String gwTopic = BASE_TOPIC + String ("/gateway/status");
         // Attempt to connect
-        if (client.connect (clientId.c_str (),MQTT_USER,MQTT_PASS)) {
+#ifdef SECURE_MQTT
+		setClock ();
+#endif
+		if (client.connect (clientId.c_str (),MQTT_USER,MQTT_PASS,gwTopic.c_str(),0,1,"0",true)) {
             Serial.println ("connected");
             // Once connected, publish an announcement...
-            String gwTopic = BASE_TOPIC + String("/gateway/hello");
-            client.publish (gwTopic.c_str(), "");
+            //String gwTopic = BASE_TOPIC + String("/gateway/hello");
+            client.publish (gwTopic.c_str(), "1");
             // ... and resubscribe
-            client.subscribe ("inTopic");
+			String dlTopic = BASE_TOPIC + String ("/+/set/#");
+            client.subscribe (dlTopic.c_str());
+			client.setCallback (onDlData);
         }
         else {
             Serial.print ("failed, rc=");
@@ -61,8 +101,11 @@ void setup()
         delay (500);
         Serial.print (".");
     }
-    randomSeed (micros ());
-    client.setServer (mqtt_server, 1883);
+#ifdef SECURE_MQTT
+	randomSeed (micros ());
+	espClient.setTrustAnchors (&cert);
+#endif
+	client.setServer (mqtt_server, mqtt_port);
 
 }
 
@@ -82,7 +125,9 @@ void loop()
 
     while (Serial.available () != 0) {
         message = Serial.readStringUntil ('\n');
+#ifdef BRIDGE_DEBUG
         Serial.printf ("Message: %s\n", message.c_str());
+#endif
         dataPresent = true;
         if (message.length () > 0) {
             break;
@@ -92,31 +137,45 @@ void loop()
     if (dataPresent) {
         dataPresent = false;
         if (message[0] == '~') {
+#ifdef BRIDGE_DEBUG
             Serial.println ("New message");
+#endif
             int end = message.indexOf (';');
             topic = BASE_TOPIC + message.substring (1, end);
+#ifdef BRIDGE_DEBUG
             Serial.printf ("Topic: %s", topic.c_str ());
+#endif
             if (end < message.length () - 1) {
-                Serial.println (" With data");
                 data = message.substring (end + 1);
-                Serial.printf (" -- Data: %s\n", data.c_str ());
+#ifdef BRIDGE_DEBUG
+				Serial.println (" With data");
+				Serial.printf (" -- Data: %s\n", data.c_str ());
+#endif
             }
             else {
-                Serial.println (" Without data");
-                data = "";
-                Serial.println ();
+               data = "";
+#ifdef BRIDGE_DEBUG
+			   Serial.println (" Without data");
+               Serial.println ();
+#endif
             }
+#ifdef BRIDGE_DEBUG
             Serial.printf ("Publish %s : %s\n",topic.c_str(), data.c_str());
-            if (client.beginPublish (topic.c_str (), data.length (), false)) {
+#endif
+			if (client.beginPublish (topic.c_str (), data.length (), false)) {
                 client.write ((const uint8_t *)data.c_str (), data.length ());
                 if (client.endPublish() == 1) {
+#ifdef BRIDGE_DEBUG
                     Serial.println ("Publish OK");
                 } else {
                     Serial.println ("Publish error");
-                }
-            } else {
+#endif
+				}
+#ifdef BRIDGE_DEBUG
+			} else {
                 Serial.println ("Publish error");
-            }
+#endif
+			}
         }
     }
 
