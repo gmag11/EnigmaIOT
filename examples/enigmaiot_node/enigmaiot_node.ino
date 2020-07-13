a6ad793905f0120e39ea058a70e608f09515a22e
Germán Martín <gmag11@gmail.com>
2020-07-13 17:59:50 +0200

    Set node address in main node code

--- examples/enigmaiot_node/enigmaiot_node.ino
+++ examples/enigmaiot_node/enigmaiot_node.ino
@@ -27,6 +27,7 @@
 #include <SPIFFS.h>
 #include <Update.h>
 #include <driver/adc.h>
+#include "esp_wifi.h"
 #endif
 #include <ArduinoJson.h>
 #include <Curve25519.h>
@@ -105,6 +106,21 @@ void setup () {
 	EnigmaIOTNode.onDataRx (processRxData);
 
 	EnigmaIOTNode.begin (&Espnow_hal);
+
+
+	uint8_t macAddress[ENIGMAIOT_ADDR_LEN];
+#ifdef ESP8266
+	if (wifi_get_macaddr (STATION_IF, macAddress))
+#elif defined ESP32
+	if ((esp_wifi_get_mac (WIFI_IF_STA, macAddress) == ESP_OK))
+#endif
+	{
+		EnigmaIOTNode.setNodeAddress (macAddress);
+		char macStr[ENIGMAIOT_ADDR_LEN * 3];
+		DEBUG_DBG ("Node address set to %s", mac2str (macAddress, macStr));
+	} else {
+		DEBUG_WARN ("Node address error");
+	}
 
 	// Put here your code to read sensor and compose buffer
 	CayenneLPP msg (MAX_DATA_PAYLOAD_SIZE);
