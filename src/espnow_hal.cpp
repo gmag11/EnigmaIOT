f5f0217deb082d1aab29e72da78efad4aa0b5bb6
Germán Martín <github@gmartin.net>
2021-12-26 13:23:31 +0100

    Add promiscuous packet types to ESP8266

--- src/espnow_hal.cpp
+++ src/espnow_hal.cpp
@@ -37,6 +37,41 @@ typedef struct {
     } vendor_specific_content;
 } __attribute__ ((packed)) espnow_frame_format_t;
 
+#ifdef ESP8266
+
+typedef struct {
+    signed rssi : 8;
+    unsigned rate : 4;
+    unsigned is_group : 1;
+    unsigned : 1;
+    unsigned sig_mode : 2;
+    unsigned legacy_length : 12;
+    unsigned damatch0 : 1;
+    unsigned damatch1 : 1;
+    unsigned bssidmatch0 : 1;
+    unsigned bssidmatch1 : 1;
+    unsigned MCS : 7;
+    unsigned CWB : 1;
+    unsigned HT_length : 16;
+    unsigned Smoothing : 1;
+    unsigned Not_Sounding : 1;
+    unsigned : 1;
+    unsigned Aggregation : 1;
+    unsigned STBC : 2;
+    unsigned FEC_CODING : 1;
+    unsigned SGI : 1;
+    unsigned rxend_state : 8;
+    unsigned ampdu_cnt : 8;
+    unsigned channel : 4;
+    unsigned : 12;
+} wifi_pkt_rx_ctrl_t;
+
+typedef struct {
+    wifi_pkt_rx_ctrl_t rx_ctrl;
+    uint8_t payload[0]; /* ieee80211 packet buff */
+} wifi_promiscuous_pkt_t;
+#endif
+
 Espnow_halClass Espnow_hal;
 
 peerType_t _peerType;
